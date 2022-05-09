92686bb1405606a76b18ef89e1a4586ab8af4bac
dingy649 <98331171+dingy649@users.noreply.github.com>
2022-05-09 18:26:32 -0400

    Final

--- sketch.js
+++ sketch.js
@@ -1,113 +1,77 @@
-let playerX = 200;
-let playerY = 500;
-let speed;
-let enemy1X = 100;
-let enemy2X = 250;
-let enemy3X = 350;
-let enemyY = 100;
-let score;
-let gameover;
-let character;
-let taddy;
-let unicorm;
-let star;
+let bullets=[];
+let enemies=[];
+let score=0;
 let outcome;
-let room;
-
+let character;
+let weapon;
+let monster;
 
 function preload(){
- character=loadImage('shi.png')
- taddy=loadImage('sha.png')
-  unicorn=loadImage('she.png')
-  star=loadImage('shs.png')
-  room =loadImage('shs6.png')
-  outcome =loadImage('si.png')
+outcome =loadImage('si.png')
+  character=loadImage('shi.png')
+  weapon =loadImage('she.png')
+  monster =loadImage('sha.png')
 }
 
 function setup() {
-  createCanvas(400, 600);
-  score = 0;
-  speed = 2;
-  gameover = false;
-  
+  createCanvas(800, 800);
+  for (let i = 0; i<5;i++){
+    let enemy={
+      x: random(0,width),
+      y: random(-1000,0)
+    }
+    enemies.push(enemy)
+  }
 }
 
 function draw() {
   background(0);
-  image(room,0,0,400,600);
-  
-  if(gameover == false)
-  {
-  collision();
-    
-  //set the player/character
-    image(character,playerX,playerY,85,100)
-    
-  // set the enemies
-   image(taddy,enemy1X,enemyY,80,90);
-   image(unicorn,enemy2X,enemyY,85,90);
-   image(star,enemy3X,enemyY,85,90);
-  
-  
-  enemyY += speed;
-  
-  if (enemyY >= height) {
-    enemyY = 0;
-    score += 1;
-    enemy1X = random(width);
-    enemy2X = random(width);
-    enemy3X = random(width);
-  }
-
-  //set the score
-  fill(255);
-  textSize(40);
-  text(score, width / 12, 50);
-    
-  } 
-  //Game over pop out
-    if(gameover == true)
-  {
-  score = 0;
-  speed = 0;
-  background(0)
+  rectMode(CENTER);
     imageMode(CENTER);
-    image(outcome,200,200,300,300);
-  textSize(50);
-  fill(250, 10, 20);
-  text('Game Over', width / 6, height / 2);
+  image(character,mouseX, height-50,100,100);
+  //draw bullets
+  for(let bullet of bullets){
+    image(weapon,bullet.x,bullet.y,20,20);
+    bullet.y -=10;
+  }
+  //draw enemies
+  for (let enemy of enemies){
+    enemy.y +=2
+    image(monster,enemy.x,enemy.y,40,40);
+    if (enemy.y>height){
     
+    background(0)
   
+    image(outcome,width/2,height/2,500,500);
+
+ 
+    }
   }
-  
-} 
-//control the movement of the character
-function keyPressed() {
- if (keyCode === LEFT_ARROW) {
-    playerX -= speed+5;
+  //deal with collision
+  for (let enemy of enemies){
+    for (let bullet of bullets){
+      if(dist(enemy.x, enemy.y, bullet.x, bullet.y)<10){
+        enemies.splice(enemies.indexOf(enemy),1)
+        bullets.splice(bullets.indexOf(bullet),1)
+         let Newenemy={
+      x: random(0,width),
+      y: random(-800,0)
+    }
+    enemies.push(Newenemy)
+        score +=1;
+        
+      }
+    }
   }
-  if (keyCode === RIGHT_ARROW) {
-    playerX += speed+5;
-  }
+  fill(255);
+  text(score,15,25);
 }
 
-//set collison
-function collision() {
-  if (playerX >= enemy1X && playerX <= enemy1X+50 && playerY >= enemyY && playerY <= enemyY + 50) {
-    score = 0;
-    gameover = true;
-    
+function mousePressed(){
+  //fire bullets when users click
+  let bullet={
+    x: mouseX,
+    y: height-50
   }
-
-  if (playerX >= enemy2X && playerX <= enemy2X+50 && playerY >= enemyY && playerY <= enemyY+50){
-    score = 0;
-     gameover = true;
-    
-  }
-
-  if (playerX >= enemy3X && playerX <= enemy3X+50  && playerY >= enemyY && playerY <= enemyY+50 ) {
-    score = 0;
-     gameover = true;
-    
-  }
+  bullets.push(bullet)
 }
